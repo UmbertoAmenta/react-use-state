6cf33347307d5b65121203f3a5f9c848fdc64e17
Umberto Amenta <umberto.amenta@hotmail.it>
2025-02-01 19:26:26 +0100

    feat: +static accordion

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -1,20 +1,21 @@
+import languages from "../data/languages";
+
 export default function Accordions() {
   return (
     <div className="accordionList">
-      <div className="accordion">
-        <div>
-          <h3>linguaggio</h3>
-          <button>+</button>
-        </div>
-        <div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
-            eaque necessitatibus mollitia distinctio qui, molestias ad quasi
-            ipsa eligendi consequatur, temporibus animi provident magni
-            doloribus? Ab necessitatibus aspernatur itaque et!
-          </p>
-        </div>
-      </div>
+      {languages.map((language) => {
+        return (
+          <div key={language.id} className="accordion">
+            <div>
+              <h3>{language.title}</h3>
+              <button>+</button>
+            </div>
+            <div>
+              <p>{language.description} </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
