3768c268ee92c403069d3354183af9491dc4d321
Umberto Amenta <umberto.amenta@hotmail.it>
2025-02-01 17:59:41 +0100

    init project

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import Accordions from "./components/Accordions";
+
+export default function App() {
+  return <Accordions />;
+}
---
+++ src/components/Accordions.jsx
@@ -0,0 +1,20 @@
+export default function Accordions() {
+  return (
+    <div className="accordionList">
+      <div className="accordion">
+        <div>
+          <h3>linguaggio</h3>
+          <button>+</button>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+            eaque necessitatibus mollitia distinctio qui, molestias ad quasi
+            ipsa eligendi consequatur, temporibus animi provident magni
+            doloribus? Ab necessitatibus aspernatur itaque et!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
